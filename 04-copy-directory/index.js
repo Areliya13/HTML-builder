768545de2f9d59d0f37fb2e22e1451d124dd10ca
Areliya13 <sharipovatamara@gmail.com>
2022-11-07 20:27:39 +0300

    fix: clear new directory before writing

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,30 +1,30 @@
 const path = require('path');
 const fs = require('fs');
-const fsPromise = require('fs/promises');
-const { stdin, stdout } = require('process');
+const { stdout } = require('process');
 
 const folder = path.join(__dirname, './files');
 const newFolder = path.join(__dirname, './files-copy');
 
-
-try {
-  const createDir = fsPromise.mkdir(newFolder, { recursive: true });
-} catch (err) {
-  stdout.write(err.message);
-}
-
-fs.readdir(folder, {withFileTypes: true}, (err, dirents) => {
-  if (err) throw err;
-  dirents.forEach((dirent) => {
-    if (dirent.isFile()) {
-      const from = path.join(folder, dirent.name);
-      const to = path.join(newFolder, dirent.name);
-      try {
-      fsPromise.copyFile(from, to);
-      }
-      catch (err) {
-        stdout.write(err.message);
-      }
-    }
-  })
+fs.rm(newFolder, {recursive: true, force: true}, (err) => {
+  if (err) stdout.write(err.message);
+  // stdout.write('Clear\n');
+  fs.mkdir(newFolder, {recursive: true}, (err) => {
+    if (err) stdout.write(err.message);
+    // stdout.write('New directory\n');
+    fs.readdir(folder, {withFileTypes: true}, (err, files) => {
+      // stdout.write('Make it\n');
+      if (err) stdout.write(err.message);
+      // stdout.write('And it\n');
+      files.forEach(file => {
+        // stdout.write(file.name + '\n');
+        if (file.isFile()) {
+          const from = path.join(folder, file.name);
+          const to = path.join(newFolder, file.name);
+          fs.copyFile(from, to, err => {
+            if (err) stdout.write(err.message);
+          });
+        }
+      });
+    });
+  });
 });
